d5846f42b9c710f9a9f3abc06d1794da37b3e55d
7yadavrupesh <7yadavrupesh@gmail.com>
2021-12-29 19:07:45 +0530

    diameter of binary tree

--- Binary Tree Interview Questions/Diameter of a binary tree.cpp
+++ Binary Tree Interview Questions/Diameter of a binary tree.cpp
@@ -10,22 +10,24 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-int helper(TreeNode* root, int& maxDia) {
-    if(!root) return 0;
-    int l = helper(root->left, maxDia);
-    int r = helper(root->right, maxDia);
-    int currentDia = l + r + 1;
-    maxDia = max(maxDia, currentDia);
-    return max(l, r) + 1;
-}
-
 class Solution {
   public:
+
+    int helper(TreeNode* root, int& maxDia) {
+        if(!root) return 0;
+        int lh = helper(root->left, maxDia);
+        int rh = helper(root->right, maxDia);
+        int currentDia = lh + rh + 1;
+        maxDia = max(maxDia, currentDia);
+        return max(lh, rh) + 1;
+    }
+
     int diameter(TreeNode* root) {
         int maxDia = INT_MIN; 
         helper(root, maxDia);
         return maxDia;
     }
+    
 };
 
 int main() {
