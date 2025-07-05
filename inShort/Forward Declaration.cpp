684825985f9db2199024bd9431bcf44964429ac6
cppnuts-yt <7yadavrupesh@gmail.com>
2025-07-05 18:17:28 +0530

    commit

---
+++ inShort/Forward Declaration.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// Forward declaration
+class B;
+
+class A {
+public:
+    void doSomething(B* b);
+};
+
+void A::doSomething(B* b) {
+    b->sayHello();
+}
+
+class B {
+public:
+    void sayHello() {
+        cout << "Hello from B!\n";
+    }
+};
+
+
+
+int main() {
+    A a;
+    B b;
+    a.doSomething(&b);
+    return 0;
+}
